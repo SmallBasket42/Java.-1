62efa30ed79a6d571a0aabd86167df1f6f7222c2
SmallBasket42 <alexander.mukha23@gmail.com>
2019-03-03 23:56:30 +0300

    Домашнее задание к уроку 2 Java Уровень 1

--- Main.java
+++ Main.java
@@ -1,116 +1,73 @@
-package Home_work_1;
-
-import java.util.Scanner;
+package Home_work_2;
 
 public class Main {
+/*1. Задать целочисленный=int массив, состоящий из элементов 0 и 1.
+    Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+    С помощью цикла for и условия заменить 0 на 1, 1 на 0*/
 
     public static void main(String[] args) {
+       /* int[] array = {1, 1, 0, 0, 1, 0};
+        int arrll = array.length;
+        for (int i = 0; i < arrll; i++) {
+            if (array[i] == 1) array[i] = 0;
+            else array[i] = 1;
+        }
+        System.out.println(" ");
+        for (int i = 0; i < arrll; i++) {
+            System.out.println(i + " " + array[i]);
+        }*/
 
-   // byte getbyte;
-    //getbyte = 2;
+       /* 2. Задать пустой целочисленный массив размером 8.
+        С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21*/
 
-    //short code = 2345;
+      /*  int[] array = new int[8];
+        int j = 0;
+        int fill = array.length;
+        System.out.println(" ");
+        System.out.println(" ");
 
-    //int min = -2342;
-
-    //long nights = 365;
-    //long seconds;
-    //seconds = nights * 6 * 60 * 60;
-
-    //float usd = 65.6f;
-
-    //double pi = 3.14;
-
-      //  char ch = 'L';
-        //int intCh = (int) ch;
-
-        //System.out.println("L соответствует "+ intCh);
-
-        //boolean a = true;
-
-    //String string = new String(" Какой прекрасный день ");
-
-
+        for (int i = 0; i < fill; ++i, j = j + 3) {
+            array[i] = j;
+            System.out.print(+array[i] + " ");
+            System.out.println(" ");*/
 
 
 
-        //Scanner scanner  = new Scanner(System.in);
-        //System.out.println("Введите превое число:");
-        //int a = scanner.nextInt();
-        //System.out.println("Введите второе число:");
-        //int b = scanner.nextInt();
-        //System.out.println("Введите третье число:");
-        //int c = scanner.nextInt();
-        //System.out.println("Введите четвертое число:");
-        //int d = scanner.nextInt();
+       /* 3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ]
+        пройти по нему циклом, и числа меньшие 6 умножить на 2.*/
 
-        //int result;
+       /* int[] array3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        int arr3 = array3.length;
+        for (int i = 0; i < arr3; i++) {
+            if (array3[i] < 6)
+                array3[i] = array3[i] * 2;
+        }
+        System.out.println(" ");
+        System.out.print(" ");
 
-        //result = a * (b + (c / d));
+        for (int i = 0; i < arr3; i++) {
 
-        //System.out.println("Результат = " + result);
-
-
+            System.out.print(+array3[i] + ", ");*/
 
 
-//Scanner scanner = new Scanner(System.in);
-//System.out.println("Первое число:");
-       // int x = scanner.nextInt();
-//System.out.println("Второе число:");
-       // int y = scanner.nextInt();
+       /*4.Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+                и с помощью цикла(-ов) заполнить его диагональные элементы единицами*/
+        /*int[][] sqArr = new int[4][4];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (i < j) {
+                    sqArr[i][j] = 0;
+                } else if (i > j) {
+                    sqArr[i][j] = 1;
+                } else {
+                    sqArr[i][j] = 1;
+                }
+                System.out.print(sqArr[i][j] + " ");
+            }
+            System.out.println();
+            }
+        }
+    }*/
 
-       // if ((x + y)>10 && (x + y)<20 )  {
-       //     System.out.println(x + y);
-      //  } else {
-       //     System.err.println("Условие не входит в рамки");
-     //   }
-
-
-
-
-        //Scanner scanner = new Scanner(System.in);
-        //System.out.println("Введите число:");
-        //int number = scanner.nextInt();
-        //if (number>=0) {
-          //  System.out.println("Число положительное");
-        //} else
-          //  System.out.println("Число отрицательное");
-
-
-
-
-
-        //static boolean ifnegavive(int number) {       рабочую версию собрать не удалось
-          //  return num < 0;
-        //}
-
-
-    //Scanner scanner = new Scanner(System.in);
-    //System.out.println("Введите имя: ");
-    //String name = scanner.nextLine();
-    //System.out.println("Привет!, "+ name);
-
-
-
-
-
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите год: ");
-        int year = scanner.nextInt();
-
-
-        if (year %100==0 && year %400 ==0 )
-            System.out.println("Год високосный");
-
-        else if (year % 4 == 0 && year % 100 > 0)
-            System.out.println("Год високосный");
-
-        else if (year % 100 == 0)
-            System.out.println("Год обычный");
-
-        else
-            System.out.println("Год обычный");
 
     }
-}
